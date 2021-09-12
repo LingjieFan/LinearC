077eb5f69f844fed73a0e4891fc2ed15813a4af2
Lingjie Fan <1072217465@qq.com>
2021-09-12 10:21:17 +0800

    Add LinearC.c to simplify Makefile (#1)

---
+++ LinearC.c
@@ -0,0 +1,15 @@
+#include "LinearC.h"
+
+#include "MSC_Complex.c"
+
+#include "Num.c"
+#include "NumD.c"
+#include "NumDC.c"
+
+#include "Vector.c"
+#include "VectorD.c"
+#include "VectorDC.c"
+
+#include "Matrix.c"
+#include "MatrixD.c"
+#include "MatrixDC.c"
